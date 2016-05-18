7a17801af6d62c8407a7361e810817e1372c7ed6
Debopam <dev1994@connect.hku.hk>
2016-05-18 18:15:27 +0800

    testing

--- aftership.php
+++ aftership.php
@@ -454,7 +454,7 @@ if (is_woocommerce_active()) {
                         if (array_key_exists('key', $aftership_field) && $field == $aftership_field['key']) {
                             echo $aftership_field['key'] . '<br/>';
                             echo $aftership_field['id'] . '<br/>';
-                            array_push($required_fields_values, $values[$aftership_field['id']]);
+                            array_shift($required_fields_values, $values[$aftership_field['id']]);
                         }
                     }
                 }
