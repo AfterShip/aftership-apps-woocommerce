57241c4f9c4f8d3367a450bb22d8e40d5633498a
Yao Yang <y.yang@aftership.com>
2021-04-06 14:16:23 +0800

    [CNT-7408] Fix issue for generate API key

--- aftership-woocommerce-tracking.php
+++ aftership-woocommerce-tracking.php
@@ -155,6 +155,26 @@ if ( is_woocommerce_active() ) {
 				add_action( 'edit_user_profile', array( $this->actions, 'add_api_key_field' ) );
 				add_action( 'personal_options_update', array( $this->actions, 'generate_api_key' ) );
 				add_action( 'edit_user_profile_update', array( $this->actions, 'generate_api_key' ) );
+
+				register_activation_hook( __FILE__, array( $this, 'install' ) );
+			}
+
+			/**
+			 * Add manage_aftership cap for administrator
+			 * Add this to allow customers to more finely configure the permissions of the aftership plugin.
+			 */
+			public function install() {
+				global $wp_roles;
+
+				if ( class_exists( 'WP_Roles' ) ) {
+					if ( ! isset( $wp_roles ) ) {
+						$wp_roles = new WP_Roles();
+					}
+				}
+
+				if ( is_object( $wp_roles ) ) {
+					$wp_roles->add_cap( 'administrator', 'manage_aftership' );
+				}
 			}
 
 
