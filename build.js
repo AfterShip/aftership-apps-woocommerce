96918b215e04d372b861032c1e3ea00788718073
ljian1992 <314625268@qq.com>
2025-02-14 17:10:11 +0800

    change build.js

--- build.js
+++ build.js
@@ -1,21 +1,13 @@
-const fs = require('fs');
-const archiver = require('archiver');
-const path = require('path');
+(async ()=> {
+    const fs = require('fs');
+    const archiver = require('archiver');
 
-(async () => {
     // 定义需要打包的目录和文件数组
     const directories = ['assets', 'includes', 'templates', 'views', 'woo-includes'];
     const files = ['aftership.php', 'aftership-woocommerce-tracking.php', 'readme.txt'];
 
     // 创建一个输出流，用于保存生成的 zip 文件
-    const buildDir = './build';
-    const build = path.join(buildDir, 'aftership-woocommerce-tracking.zip');
-
-    // 确保目标目录存在
-    if (!fs.existsSync(buildDir)) {
-        fs.mkdirSync(buildDir);
-    }
-
+    const build = './build/aftership-woocommerce-tracking.zip';
     if (fs.existsSync(build)) {
         fs.unlinkSync(build);
     }
@@ -31,14 +23,15 @@ const path = require('path');
 
     // 循环遍历需要打包的目录，并添加到 archiver 实例中
     directories.forEach((dir) => {
-        archive.directory(dir, false);
+        archive.directory(dir);
     });
 
     // 循环遍历需要打包的文件，并添加到 archiver 实例中
     files.forEach((file) => {
-        archive.file(file, { name: file });
+        archive.file(file);
     });
 
     // 完成打包操作
     archive.finalize();
-})();
+
+})()
